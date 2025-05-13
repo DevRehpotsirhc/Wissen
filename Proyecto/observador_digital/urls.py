e566128d48eaa330826f263e19ea1f189a950919
DevRehpotsirhc <christopher4ponte@gmail.com>
2025-05-13 12:59:37 -0500

    feature: nueva estructura del proyecto, dejando de lado las APIs y dejando el front dentro del propio Django

--- Proyecto/observador_digital/urls.py
+++ Proyecto/observador_digital/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for apis project.
+URL configuration for observador_digital project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
