d47928aa9169b5391b7bcc17cbcd7221f6ec4b63
Elmer1213 <elmer.macias@cun.edu.co>
2025-05-15 17:53:19 -0500

    add: añadi los modelos del generador de informes

--- Proyecto/gen_infos/models.py
+++ Proyecto/gen_infos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from usuarios.models import Administrador, Docente, Estudiante
+
 
 # Create your models here.
+class GenInfo(models.Model):
+    id_gen_info = models.AutoField(primary_key=True)
+    id_tipo = models.ForeignKey('Tipo', on_delete=models.CASCADE)
+    id_docente = models.ForeignKey('Docente', on_delete=models.CASCADE, null=True)
+    id_admin = models.ForeignKey('Admin', on_delete=models.CASCADE, null=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.id_docente:
+            return f"Info Docente: {self.id_docente}"
+        elif self.id_admin:
+            return f"Info Admin: {self.id_admin}"
+        return f"GenInfo #{self.id_gen_info}"
+
+class Tipo(models.Model):
+    id_tipo = models.AutoField(primary_key=True)
+    tipo = models.CharField(max_length=5)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.tipo
