ea8dc2e4f116e7906a44f462b0ba14310614b105
DevRehpotsirhc <christopher4ponte@gmail.com>
2025-05-19 05:53:35 -0500

    add: añadí una lógica básica de prueba para el formulario de registro del modelo de Persona

--- Proyecto/usuarios/views.py
+++ Proyecto/usuarios/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import PersonaForm
+
 
 # Create your views here.
+def persona_form(request):
+    if request.method == 'POST':
+        form = PersonaForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = PersonaForm()
+    return render(request, 'form_persona.html', {'form': form})
