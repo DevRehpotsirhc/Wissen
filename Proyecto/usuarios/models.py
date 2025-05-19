d4c7a745a9697d1f6fe18aefcabd07ae5ec94b97
DevRehpotsirhc <christopher4ponte@gmail.com>
2025-05-19 05:54:58 -0500

    refactor: cambié la lógica de los modelos y eliminé tablas innecesarias

--- Proyecto/usuarios/models.py
+++ Proyecto/usuarios/models.py
@@ -4,58 +4,21 @@ from django.db import models
 
 
 
-# Modelo de Tipo de Identificación
-class TipoIden(models.Model):
-    id_tipo_iden = models.AutoField(primary_key=True)
-    tipo_iden = models.CharField(max_length=50)
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-    
-    # Estructura de escritura que se mostrará al llamar al modelo
-    def __str__(self):
-        return f"Tipo de Identificación: {self.tipo_iden}\nFecha de Creación: {self.fecha_creacion.strftime('%d/%m/%Y')}"
-
-
-
-# Modelo de Roles (Administrador, Docente, Estudiante)
-class Rol(models.Model):
-    id_rol = models.AutoField(primary_key=True)
-    rol = models.CharField(max_length=10)
-    fecha_creacion = models.DateTimeField(auto_now_add= True)
-    
-    # Estructura de escritura que se mostrará al llamar al modelo
-    def __str__(self):
-        return f"Rol: {self.rol}\nFecha de Creación: {self.fecha_creacion.strftime('%d/%m/%Y')}"
-
-
-
-# Modelo de Estado en el sistema (Activo, Inactivo, En revisión)
-class Estado(models.Model):
-    id_estado = models.AutoField(primary_key=True)
-    estado = models.CharField(max_length=10)
-    fecha_creacion = models.DateTimeField(auto_now_add= True)
-    
-    # Estructura de escritura que se mostrará al llamar al modelo
-    def __str__(self):
-        return f"Estado en el Sistema: {self.estado}\nFecha de Creación: {self.fecha_creacion.strftime('%d/%m/%Y')}"
-
-
-
 # Modelo que almacena todos los datos de una persona independientemente su rol
 class Persona(models.Model):
     id_persona = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
     apellidos = models.CharField(max_length=50)
-    # Evita que se elimine un tipo de identificacion si esta relacionado con alguna persona.
-    id_tipo_iden = models.ForeignKey(TipoIden, on_delete=models.CASCADE)
+    IDEN = [
+        ('ti', 'Tarjeta de Identidad'),
+        ('ci', 'Cédula de Identidad'),
+        ('ce', 'Cédula de Extrajería'),
+        ('ppt', 'Permiso por Protección Temporal'),
+    ]
+    tipo_iden = models.CharField(max_length=3, choices=IDEN, default='ti')
     num_iden = models.CharField(max_length=12, unique=True)
-    # Evita que se elimine un rol que se esté en uso
-    id_rol = models.ManyToManyField(Rol, related_name="persona_rol")
-    foto = models.CharField(max_length=250, blank=True, null=True)
-    # blank = True: Permite que el campo este vacio en formulario
-    # Permite que se guarde como null en la base de datos
     correo = models.EmailField(max_length=254)
     tel = models.CharField(max_length=15)
-    id_estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
     # Automatico cuando se crea el registro
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     
@@ -63,7 +26,7 @@ class Persona(models.Model):
     def __str__(self):
         # Validar tipo de identificación y darle un nombre dependiendo el id que tenga
         # Validar tipo estado y darle un nombre dependiendo el id que tenga
-        return f"Nombre: {self.nombre}\nApellido: {self.apellidos}\nTipo de Identificación: {self.id_tipo_iden}\nNúmero de Identificación: {self.num_iden}\nRol: {self.id_rol}\nRuta de la Foto: {self.foto}\nCorreo: {self.correo}\nTeléfono: {self.tel}\nEstado en el Sistema: {self.id_estado}\nFecha de Creación: {self.fecha_creacion.strftime('%d/%m/%Y')}"
+        return f"Nombre: {self.nombre}\nApellido: {self.apellidos}\nTipo de Identificación: {self.tipo_iden}\nNúmero de Identificación: {self.num_iden}\nCorreo: {self.correo}\nTeléfono: {self.tel}\nFecha de Creación: {self.fecha_creacion.strftime('%d/%m/%Y')}"
 
 
 
@@ -72,24 +35,27 @@ class Usuario(models.Model):
     id_usuario = models.AutoField(primary_key=True)
     usuario = models.CharField(max_length=50, unique=True)
     clave = models.CharField(max_length=255)
+    ROLES = [
+        ('admin', 'Administrador'),
+        ('docen', 'Docente'),
+        ('estud', 'Estudiante'),
+    ]
+    rol = models.CharField(max_length=5, choices=ROLES, default='estud')
+    ESTADOS = [
+        ('act', 'Activo'),
+        ('ina', 'Inactivo'),
+        ('rev', 'En Revisión'),
+    ]
+    # blank = True: Permite que el campo este vacio en formulario
+    # Permite que se guarde como null en la base de datos
+    foto = models.CharField(max_length=250, blank=True, null=True)
+    estado = models.CharField(max_length=3, choices=ESTADOS, default='act')
     id_persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     
     # Estructura de escritura que se mostrará al llamar al modelo
     def __str__(self):
         return f"Usuario: {self.usuario}\nClave: {self.clave}\nPersona: {self.id_persona}\nFecha de Creación: {self.fecha_creacion.strftime('%d/%m/%Y')}"
-
-
-
-# Modelo de cargos administrativos (rector, subdirector, director, etc)
-class Cargo(models.Model):
-    id_cargo = models.AutoField(primary_key=True)
-    cargo = models.CharField(max_length=30)
-    fecha_creacion = models.DateTimeField(auto_now_add= True)
-    
-    # Estructura de escritura que se mostrará al llamar al modelo
-    def __str__(self):
-        return f"Cargo: {self.cargo}\nFecha de Creación: {self.fecha_creacion.strftime('%d/%m/%Y')}"
     
 
 
@@ -97,12 +63,17 @@ class Cargo(models.Model):
 class Administrador(models.Model):
     id_administrador = models.AutoField(primary_key=True)
     id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    id_cargo = models.ForeignKey(Cargo, on_delete=models.CASCADE)
+    CARGOS = [
+        ('rec', 'Rector'),
+        ('dir', 'Director'),
+        ('adm', 'Administrador'),
+    ]
+    cargo = models.CharField(max_length=50, choices=CARGOS, default='adm')
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     
     # Estructura de escritura que se mostrará al llamar al modelo
     def __str__(self):
-        return f"Usuario: {self.id_usuario}\nCargo: {self.id_cargo}\nFecha de Creación: {self.fecha_creacion.strftime('%d/%m/%Y')}"
+        return f"Usuario: {self.id_usuario}\nCargo: {self.cargo}\nFecha de Creación: {self.fecha_creacion.strftime('%d/%m/%Y')}"
     
 
 
@@ -115,31 +86,25 @@ class Materia(models.Model):
     # Estructura de escritura que se mostrará al llamar al modelo
     def __str__(self):
         return f"Materia: {self.materia}\nFecha de Creación: {self.fecha_creacion.strftime('%d/%m/%Y')}"
-
-
-
-# Modelo de materias (matemáticas, física, química, etc)
-class Jornada(models.Model):
-    id_jornada = models.AutoField(primary_key=True)
-    jornada = models.CharField(max_length=20)
-    fecha_creacion = models.DateTimeField(auto_now_add= True)
-    
-    # Estructura de escritura que se mostrará al llamar al modelo
-    def __str__(self):
-        return f"Jornada: {self.jornada}\nFecha de Creación: {self.fecha_creacion.strftime('%d/%m/%Y')}"
     
 
 
-# Modelo de cursos y/o grados (1ro, 2do, 3ro, etc), con relación 1 a muchos con jornada
+# Modelo de cursos y/o grados (1ro, 2do, 3ro, etc)
 class Curso(models.Model):
     id_curso = models.AutoField(primary_key=True)
     curso = models.CharField(max_length=50)
-    id_jornada = models.ForeignKey(Jornada, on_delete=models.CASCADE)
+    JORNADA = [
+        ('man', 'Mañana'),
+        ('tar', 'Tarde'),
+        ('noc', 'Noche'),
+        ('sab', 'Sabatino'),
+    ]
+    jornada = models.CharField(max_length=50, choices=JORNADA, default='man')
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     
     # Estructura de escritura que se mostrará al llamar al modelo
     def __str__(self):
-        return f"Curso: {self.curso}\nJornada: {self.id_jornada}\nFecha de Creación: {self.fecha_creacion.strftime('%d/%m/%Y')}"
+        return f"Curso: {self.curso}\nJornada: {self.jornada}\nFecha de Creación: {self.fecha_creacion.strftime('%d/%m/%Y')}"
 
 
 
